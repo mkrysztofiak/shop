11995ca8cd8a87fb1df2f2d8b60cc19e51c22f64
Mateusz Krysztofiak <mateusz@krysztofiak.net>
2019-10-11 22:31:50 +0200

    Product entity init and db migration

---
+++ src/Entity/Product.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="decimal", precision=12, scale=2)
+     */
+    private $priceGross;
+
+    /**
+     * @ORM\Column(type="string", length=3)
+     */
+    private $priceCurrency;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPriceGross(): ?string
+    {
+        return $this->priceGross;
+    }
+
+    public function setPriceGross(string $priceGross): self
+    {
+        $this->priceGross = $priceGross;
+
+        return $this;
+    }
+
+    public function getPriceCurrency(): ?string
+    {
+        return $this->priceCurrency;
+    }
+
+    public function setPriceCurrency(string $priceCurrency): self
+    {
+        $this->priceCurrency = $priceCurrency;
+
+        return $this;
+    }
+}
